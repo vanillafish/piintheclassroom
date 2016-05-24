fa9a31fa31646839be097ded163bdd3683f9c472
Mike Renne <triadcodeclub@gmail.com>
2016-05-24 15:42:28 -0400

    Update guess_number.py

--- python/guess_number.py
+++ python/guess_number.py
@@ -1,1 +1,13 @@
-python code ...
+import random
+num = random.randint(1,100)
+while True:
+    print('Guess a number between 1 and 100')
+    guess = input()
+    i = int(guess)
+    if i == num:
+        print("You guessed right!")
+        break
+    elif i < num:
+        print("Higher")
+    elif i > num:
+        print("Lower")
